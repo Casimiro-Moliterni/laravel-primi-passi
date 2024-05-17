633e2e999968fb9473fa0119bcc65bebb6f5d535
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-05-17 16:37:24 +0200

    add file view home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'test';
+ return view('home');
+});
+
+Route::get('/about',function(){
+    return 'ciao about';
 });
